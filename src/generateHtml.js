ebb31e2f3f7a3ad3a020ef1ff1c823362ca8749d
park-d <54589582+park-d@users.noreply.github.com>
2022-09-26 16:49:46 -0500

    added in the basic html content for the page, in the src folder

---
+++ src/generateHtml.js
@@ -0,0 +1,31 @@
+const generateAllHtml = (teamHtml) => {
+    return `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css" integrity="sha384-xeJqLiuOvjUBq3iGOjvSQSIlwrpqjSHXpduPd6rQpuiM3f5/ijby8pCsnbu5S81n" crossorigin="anonymous">
+    <title>Team Profile Generator</title>
+</head>
+<body>
+<div class="container-fluid pb-5">
+    <div class="row d-flex justify-content-center">
+        <div class="col-12 jumbotron bg-danger text-white py-5">
+            <h1 class="text-center">My Team</h1>
+        </div>
+    </div>
+</div>   
+<div class="container col-10">
+    <div class="row d-flex justify-content-center">              
+    <!--rendered content will go here-->
+    </div>
+</div>     
+</body>
+</html>`;
+};
+
+module.exports = {generateAllHtml};
