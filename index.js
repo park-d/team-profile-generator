06cabfa1d305342c982f2f937afb060ab0ff5888
park-d <54589582+park-d@users.noreply.github.com>
2022-09-25 20:47:29 -0500

    added the engineer inquierer prompt

--- index.js
+++ index.js
@@ -37,10 +37,18 @@ function buildTeam() {
                 choices: ["Engineer", "Intern", "I don't want to add any more team members"]
             },
         ])
-        .then((addTeamMem) => {
-            switch(addTeamMem.addTeamMem) {
+        .then((results) => {
+            const managerObj = new Manager(
+                results.managerName,
+                results.managerId,
+                results.managerEmail,
+                results.managerOfficeNum,
+            );
+            console.log(managerObj);
+            switch(results.addTeamMem) {
                 case "Engineer":
                     console.log("run a function that runs engineer questions");
+                    addEngineer();
                     break;
                 case "Intern":
                     console.log("run a function that runs intern questions");
@@ -50,8 +58,56 @@ function buildTeam() {
                     break;
                 default:
                     return;
-        }
-    })
+            }
+        });
 };
 
 buildTeam();
+
+function addEngineer() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "engineerName",
+                message: "What is your engineer's name?",
+            },
+            {
+                type: "input",
+                name: "engineerId",
+                message: "What is your engineer's ID?",
+            },
+            {
+                type: "input",
+                name: "engineerEmail",
+                message: "What is your engineer's email?",
+            },
+            {
+                type: "input",
+                name: "engineerGitHub",
+                message: "What is your engineer's GitHub username?",
+            },
+            {
+                type: "list",
+                name: "addTeamMem",
+                message: "Which type of team member would you like to add?",
+                choices: ["Engineer", "Intern", "I don't want to add any more team members"]
+            },
+        ])
+        .then((addTeamMem) => {
+            switch(addTeamMem.addTeamMem) {
+                case "Engineer":
+                    console.log("run a function that runs engineer questions");
+                    addEngineer();
+                    break;
+                case "Intern":
+                    console.log("run a function that runs intern questions");
+                    break;
+                case "I don't want to add any more team members":
+                    console.log("generate HTML");
+                    break;
+                default:
+                    return;
+            }
+        });
+};
